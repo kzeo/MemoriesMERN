67ee63c2978ec3280a816a24c54a013607c6cce6
kzo <bartosz.krzeminski24@gmail.com>
2022-07-17 22:04:17 +0200

    1
    1

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getPosts } from '../controllers/posts.js'
+import { getPosts, createPost } from '../controllers/posts.js'
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
   router.get('/', getPosts)
+  router.get('/', createPost)
 })
 
 export default router
