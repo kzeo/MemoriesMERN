b0ad2abc0ef09c79c6ccb0f64b16e5ad44acbf86
kzo <bartosz.krzeminski24@gmail.com>
2022-07-18 21:13:03 +0200

    update
    update

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
-
+import { createStore, applyMiddleware, compose } from 'redux'
+import thunk from 'redux-thunk'
+import reducers from './reducers'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const store = createStore(reducers, compose(applyMiddleware(thunk)))
+
+const rootElement = document.getElementById('root')
+const root = createRoot(rootElement)
+
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+)
--- server/index.js
+++ server/index.js
@@ -7,11 +7,10 @@ import postRoutes from './routes/posts.js'
 
 const app = express()
 
-app.use('/posts', postRoutes)
-
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors())
+app.use('/posts', postRoutes)
 
 const CONNECTION_URL =
   'mongodb+srv://kzo:71WJXAGZU9wgk19V@memoriesdb.92v9a.mongodb.net/?retryWrites=true&w=majority'
