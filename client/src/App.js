22c73848360bb9cc81e627feffbec44a8682d5e1
kzo <87224332+kzeo@users.noreply.github.com>
2022-07-18 11:45:38 +0200

    Structure
    New folders are made to develope complex tree. Need to put redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,43 @@
 import React from 'react'
+import { Container, AppBar, Typography, Grow, Grid } from '@material-ui/core'
+import memories from './images/memories.png'
+import Posts from './components/Posts/Posts.js'
+import Form from './components/Form/Form.js'
+import useStyles from './styles.js'
 
 const App = () => {
+  const classes = useStyles()
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <Container maxwidth="lg">
+      <AppBar className={classes.AppBar} position="static" clolr="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          Memomries
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="memories"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={4}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   )
 }
 
