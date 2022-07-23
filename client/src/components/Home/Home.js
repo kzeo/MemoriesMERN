3ed1dc6ae4a805590551ccdedc6338432e193568
kzo <bartosz.krzeminski24@gmail.com>
2022-07-23 19:10:02 +0200

    Auth
    putting LogIn option

---
+++ client/src/components/Home/Home.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { Container, Grow, Grid } from '@material-ui/core'
+import Posts from '../Posts/Posts.js'
+import Form from '../Form/Form.js'
+import { useDispatch } from 'react-redux'
+import { getPosts } from '../../actions/posts'
+import useStyles from '../../styles.js'
+
+const Home = () => {
+  const [currentId, setCurrentId] = useState(0)
+  const classes = useStyles()
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getPosts())
+  }, [currentId, dispatch])
+
+  return (
+    <Grow in>
+      <Container>
+        <Grid
+          className={classes.mainContainer}
+          container
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={7}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={4}>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  )
+}
+
+export default Home
