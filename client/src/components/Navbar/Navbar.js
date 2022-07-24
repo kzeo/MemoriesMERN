bca2ae1688c56431bcf587317fe099492127f437
kzo <bartosz.krzeminski24@gmail.com>
2022-07-24 17:41:53 +0200

    Log in done
    as needed

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -44,17 +44,17 @@ const Navbar = () => {
         <img className={classes.image} src={memories} alt="icon" height="60" />
       </div>
       <Toolbar className={classes.toolbar}>
-        {user ? (
+        {user?.result ? (
           <div className={classes.profile}>
             <Avatar
-              className={classes.profile}
-              alt={user.result.name}
-              src={user.result.imageUrl}
+              className={classes.purple}
+              alt={user?.result.name}
+              src={user?.result.imageUrl}
             >
-              {user.result.name.charAt(0)}
+              {user?.result.name.charAt(0)}
             </Avatar>
             <Typography className={classes.userName} variant="h6">
-              {user.result.name}
+              {user?.result.name}
             </Typography>
             <Button
               variant="contained"
