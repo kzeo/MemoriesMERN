9ade0625ac4cff3273e5be59e64e15a100f05cb2
kzo <bartosz.krzeminski24@gmail.com>
2022-07-26 19:13:55 +0200

    AppBar
    AppBar = Memories title + tag + button "search" without Redux functionality

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,37 @@
 import React from 'react'
 import { Container } from '@material-ui/core'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom'
 
 import Navbar from './components/Navbar/Navbar.js'
 import Home from './components/Home/Home.js'
 import Auth from './components/Auth/Auth.js'
+import PostDetails from './components/PostDetails/PostDetails.jsx'
 
-const App = () => (
-  <Router>
-    <Container maxWidth="lg">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </Container>
-  </Router>
-)
+const App = () => {
+  const user = JSON.parse(localStorage.getItem('profile'))
+
+  return (
+    <Router>
+      <Container maxWidth="xl">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Navigate to="/posts" />} />
+          <Route path="/posts" element={<Home />} />
+          <Route path="/posts/search" element={<Home />} />
+          <Route path="/posts/:id" element={<PostDetails />} />
+          <Route
+            path="/auth"
+            element={!user ? <Auth /> : <Navigate to="/posts" />}
+          />
+        </Routes>
+      </Container>
+    </Router>
+  )
+}
 
 export default App
--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,35 +1,109 @@
 import React, { useState, useEffect } from 'react'
-import { Container, Grow, Grid } from '@material-ui/core'
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from '@material-ui/core'
+import { useNavigate, useLocation } from 'react-router-dom'
+import ChipInput from 'material-ui-chip-input'
 import Posts from '../Posts/Posts.js'
 import Form from '../Form/Form.js'
 import { useDispatch } from 'react-redux'
 import { getPosts } from '../../actions/posts'
-import useStyles from '../../styles.js'
+import Pagination from '../Pagination.jsx'
+import useStyles from './styles.js'
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search)
+}
 
 const Home = () => {
   const [currentId, setCurrentId] = useState(0)
   const classes = useStyles()
   const dispatch = useDispatch()
+  const query = useQuery()
+  const navigate = useNavigate()
+  const page = query.get('page') || 1
+  const searchQuery = query.get('searchQuery')
+  const [search, setSearch] = useState('')
+  const [tags, setTags] = useState([])
 
   useEffect(() => {
     dispatch(getPosts())
   }, [currentId, dispatch])
 
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      searchPost()
+    }
+  }
+
+  const handleAdd = (tag) => setTags([...tags, tag])
+
+  const handleDelete = (tagToDelete) =>
+    setTags(tags.filter((tag) => tag !== tagToDelete))
+
+  const searchPost = () => {
+    if (search.trim()) {
+      //dispatch -> fetch search Post
+    } else {
+      navigate('/')
+    }
+  }
+
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
-          className={classes.mainContainer}
           container
           justifyContent="space-between"
           alignItems="stretch"
           spacing={3}
+          className={classes.gridContainer}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                onKeyPress={handleKeyPress}
+                fullWidth
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <ChipInput
+                style={{ margin: '10px 0' }}
+                value={tags}
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button
+                onClick={searchPost}
+                className={classes.searchButton}
+                variant="contained"
+                color="primary"
+              >
+                Search
+              </Button>
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper elevation={6}>
+              <Pagination />
+            </Paper>
           </Grid>
         </Grid>
       </Container>
