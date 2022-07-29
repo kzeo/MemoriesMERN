63cfa4a99c57424e270b0e1e5d9bab71b308580c
kzo <bartosz.krzeminski24@gmail.com>
2022-07-29 17:33:05 +0200

    delete
    delete action fixed by adding state.posts.filter...

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -7,7 +7,7 @@ export default (state = { isLoading: true, posts: [] }, action) => {
     case 'DELETE':
       return {
         ...state,
-        posts: state.filter((post) => post._id !== action.payload),
+        posts: state.posts.filter((post) => post._id !== action.payload),
       }
     case 'UPDATE':
     case 'LIKE':
